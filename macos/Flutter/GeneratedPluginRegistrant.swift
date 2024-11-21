616fcb8ce3ea34a0ccdfb5ed3600efff4f6f49ea
EricZeller <98104805+EricZeller@users.noreply.github.com>
2024-11-21 15:04:50 +0100

    mac update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import dynamic_color
 import package_info_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
